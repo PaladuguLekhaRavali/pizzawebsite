983a6aa597701b3f4768d4f81b00928bf903048a
Lekha Ravali <lekharavalipaladugu1234@gamil.com>
2023-10-21 20:07:04 +0530

    menu added

---
+++ script.js
@@ -0,0 +1,10 @@
+let navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+}
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    // myOrders.classList.remove('active');
+    // cart.classList.remove('active');
+ };
